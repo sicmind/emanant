af0dc49a076f3a7867944324dcc01731e34eef57
sicmind <mtomasi@sicmind.com>
2022-03-04 14:13:21 -0500

    Proof of concept

--- index.js
+++ index.js
@@ -1,2 +1,43 @@
 //setup of cdn testing
-alert('works')
+const include = () => {
+    var tags, elem, temp, file 
+
+    tags = document.querySelectorAll("*[include]").forEach( elem => {
+        file = elem.getAttribute('include')
+        fetch('/Email%20Components/' + file + '.html')
+        .then( res => { return res.text() })
+        .then( html => {
+            temp = document.createElement('template');
+            temp.innerHTML = html.trim();
+            temp = temp.content.firstChild
+           // elem.removeAttribute('include')
+
+            elem.parentNode.replaceChild(temp,elem)
+        })
+    })
+
+}
+
+
+const menu = document.createElement('div')
+menu.style= 'width:100%; height:20px;background-color:#444;text-align:center;color:white'
+menu.innerHTML = 'Download'
+menu.addEventListener('click', evt => {
+    
+    document.querySelectorAll('script').forEach( s => {document.body.removeChild(s)})
+    document.body.removeChild(menu);
+    const x = new XMLSerializer().serializeToString(document)
+    var url = window.location.pathname;
+    var filename = url.substring(url.lastIndexOf('/')+1);
+    const a = document.createElement('a')
+    a.setAttribute('download', filename )
+    a.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(x))
+    document.body.appendChild(a)
+
+     a.click();
+
+})
+document.body.prepend(menu, document.body.firstChild)
+
+//initiate
+include()
